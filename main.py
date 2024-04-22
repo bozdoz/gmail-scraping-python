59e744ba580ce2fbcd7503b3a4d767fe54a9ec24
bozdoz <howaboutben@gmail.com>
2024-04-21 23:41:09 -0300

    TODO For 2025

--- main.py
+++ main.py
@@ -15,26 +15,28 @@ from googleapiclient.discovery import build
 load_dotenv()
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
-YEAR = int(os.getenv('YEAR', 2022))
-NAMES = os.getenv('NAMES', "")
+YEAR = int(os.getenv("YEAR", 2022))
+NAMES = os.getenv("NAMES", "")
 
 # your oauth credentials file
-CREDENTIALS = 'credentials.json'
+CREDENTIALS = "credentials.json"
 # generated google token file
 TOKEN = "token.json"
 
 # global google apis service
 service = None
 
+
 def get_service():
-    '''Call the Gmail API'''
+    """Call the Gmail API"""
     global service
 
     creds = get_creds()
-    
-    service = build('gmail', 'v1', credentials=creds)
+
+    service = build("gmail", "v1", credentials=creds)
+
 
 def get_creds():
     # The file token.json stores the user's access and refresh tokens, and is
@@ -48,58 +50,60 @@ def get_creds():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                CREDENTIALS, SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open(TOKEN, 'w') as token:
+        with open(TOKEN, "w") as token:
             token.write(creds.to_json())
 
     return creds
 
+
 def get_query(q: list[str]):
     if q is None:
         q = []
-    
-    default = [
-        'in:ETransfers',
-        '-mint',
-        '-communications'
-    ]
+
+    default = ["in:ETransfers", "-mint", "-communications"]
 
     q = default + q
 
-    return ' '.join(q)
+    return " ".join(q)
+
 
 def get_emails(q: list[str]):
-    '''gets thread snippets'''
+    """gets thread snippets"""
     query = get_query(q)
-    results = service.users().threads().list(
-        userId='me', 
-        q=query
-    ).execute()
-    threads = results.get('threads', [])
+    results = service.users().threads().list(userId="me", q=query).execute()
+    threads = results.get("threads", [])
 
-    return [a['snippet'].lower() for a in threads]
+    return [a["snippet"].lower() for a in threads]
+
 
 def format_date_mdy(d: datetime):
-    '''M/D/Y - used for spreadsheet'''
+    """M/D/Y - used for spreadsheet"""
     return d.strftime("%m/%d/%Y")
 
+
 def format_date(d: datetime):
-    '''Y/M/D - used for gmail querystring'''
+    """Y/M/D - used for gmail querystring"""
     return d.strftime("%Y/%m/%d")
 
+
 def get_emails_for_month(m: int):
+    # TODO: we should just get all transactions for a month
+    # otherwise, we get wrong year data
     before = datetime(YEAR, m, 15)
     prev_year = YEAR - 1 if m == 1 else YEAR
     prev_month = 12 if m == 1 else m - 1
     after = datetime(prev_year, prev_month, 15)
 
-    return get_emails([
-        'after:{0}'.format(format_date(after)),
-        'before:{0}'.format(format_date(before)),
-    ])
+    return get_emails(
+        [
+            "after:{0}".format(format_date(after)),
+            "before:{0}".format(format_date(before)),
+        ]
+    )
+
 
 def get_data_for_month(m: int):
     date = format_date_mdy(datetime(YEAR, m, 1))
@@ -109,8 +113,10 @@ def get_data_for_month(m: int):
     names = NAMES.split(", ")
 
     for name in names:
-        name_parts = name.lower().split(' ')
-        snippets = [email for email in emails if all([name in email for name in name_parts])]
+        name_parts = name.lower().split(" ")
+        snippets = [
+            email for email in emails if all([name in email for name in name_parts])
+        ]
 
         for snippet in snippets:
             money = re.findall(r"(\$\d+)\.", snippet)
@@ -119,25 +125,26 @@ def get_data_for_month(m: int):
                 output.append([name, date, money[0]])
             else:
                 print("couldn't find money: {0}".format(snippet))
-    
+
     return "\n".join([",".join(a) for a in output]) + "\n"
 
 
 def get_data_for_year():
-    out = ''
+    out = ""
 
-    for x in range(1,13):
+    for x in range(1, 13):
         out += get_data_for_month(x)
 
     return out
 
+
 def main():
     get_service()
-    
+
     output = get_data_for_year()
 
     print(output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
